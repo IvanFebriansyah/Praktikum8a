22093396bb90328aaf8a35bd9ddc9e3d4d90fc20
IvanFebriansyah <87108996+IvanFebriansyah@users.noreply.github.com>
2021-12-12 13:02:34 +0700

    Add files via upload

---
+++ Praktikum8a/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace Praktikum8a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.txtcarinama = new System.Windows.Forms.ToolStripTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtnama = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtvoucher = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.dgvcustomer = new System.Windows.Forms.DataGridView();
+            this.masakDataSet = new Praktikum8a.masakDataSet();
+            this.customerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.customerTableAdapter = new Praktikum8a.masakDataSetTableAdapters.customerTableAdapter();
+            this.idCustomerDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.namaCustomerDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nominalisasiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvcustomer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masakDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.txtcarinama,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(453, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(92, 22);
+            this.toolStripLabel1.Text = "Pencarian Data :";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // txtcarinama
+            // 
+            this.txtcarinama.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtcarinama.Margin = new System.Windows.Forms.Padding(2, 0, 1, 0);
+            this.txtcarinama.Name = "txtcarinama";
+            this.txtcarinama.Size = new System.Drawing.Size(300, 25);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id Customer";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(188, 56);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(199, 20);
+            this.txtid.TabIndex = 2;
+            // 
+            // txtnama
+            // 
+            this.txtnama.Location = new System.Drawing.Point(188, 92);
+            this.txtnama.Name = "txtnama";
+            this.txtnama.Size = new System.Drawing.Size(199, 20);
+            this.txtnama.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nama Customer";
+            // 
+            // txtvoucher
+            // 
+            this.txtvoucher.Location = new System.Drawing.Point(188, 127);
+            this.txtvoucher.Name = "txtvoucher";
+            this.txtvoucher.Size = new System.Drawing.Size(199, 20);
+            this.txtvoucher.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(51, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Isi Voucher";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Image = global::Praktikum8a.Properties.Resources.sync;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(335, 166);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 62);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Reset\r\nData";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Image = global::Praktikum8a.Properties.Resources.cross;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(225, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 62);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Delete";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Image = global::Praktikum8a.Properties.Resources.update;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(115, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 62);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Update";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = global::Praktikum8a.Properties.Resources.plus;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(17, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 62);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Insert";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // dgvcustomer
+            // 
+            this.dgvcustomer.AutoGenerateColumns = false;
+            this.dgvcustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvcustomer.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idCustomerDataGridViewTextBoxColumn,
+            this.namaCustomerDataGridViewTextBoxColumn,
+            this.nominalisasiDataGridViewTextBoxColumn});
+            this.dgvcustomer.DataSource = this.customerBindingSource;
+            this.dgvcustomer.Location = new System.Drawing.Point(17, 262);
+            this.dgvcustomer.Name = "dgvcustomer";
+            this.dgvcustomer.Size = new System.Drawing.Size(422, 287);
+            this.dgvcustomer.TabIndex = 11;
+            // 
+            // masakDataSet
+            // 
+            this.masakDataSet.DataSetName = "masakDataSet";
+            this.masakDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customerBindingSource
+            // 
+            this.customerBindingSource.DataMember = "customer";
+            this.customerBindingSource.DataSource = this.masakDataSet;
+            // 
+            // customerTableAdapter
+            // 
+            this.customerTableAdapter.ClearBeforeFill = true;
+            // 
+            // idCustomerDataGridViewTextBoxColumn
+            // 
+            this.idCustomerDataGridViewTextBoxColumn.DataPropertyName = "idCustomer";
+            this.idCustomerDataGridViewTextBoxColumn.HeaderText = "idCustomer";
+            this.idCustomerDataGridViewTextBoxColumn.Name = "idCustomerDataGridViewTextBoxColumn";
+            // 
+            // namaCustomerDataGridViewTextBoxColumn
+            // 
+            this.namaCustomerDataGridViewTextBoxColumn.DataPropertyName = "namaCustomer";
+            this.namaCustomerDataGridViewTextBoxColumn.HeaderText = "namaCustomer";
+            this.namaCustomerDataGridViewTextBoxColumn.Name = "namaCustomerDataGridViewTextBoxColumn";
+            // 
+            // nominalisasiDataGridViewTextBoxColumn
+            // 
+            this.nominalisasiDataGridViewTextBoxColumn.DataPropertyName = "nominalisasi";
+            this.nominalisasiDataGridViewTextBoxColumn.HeaderText = "nominalisasi";
+            this.nominalisasiDataGridViewTextBoxColumn.Name = "nominalisasiDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 581);
+            this.Controls.Add(this.dgvcustomer);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtvoucher);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtnama);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "CRUD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvcustomer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masakDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox txtcarinama;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtnama;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtvoucher;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dgvcustomer;
+        private masakDataSet masakDataSet;
+        private System.Windows.Forms.BindingSource customerBindingSource;
+        private masakDataSetTableAdapters.customerTableAdapter customerTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idCustomerDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn namaCustomerDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nominalisasiDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Praktikum8a/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Praktikum8a
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-ABD7H3A;Initial Catalog=masak;Integrated Security=True");
+
+        private void resetdata()
+        {
+            txtid.Text = "";
+            txtnama.Text = "";
+            txtvoucher.Text = "";
+        }
+
+        private void showdata()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM customer ORDER BY idCustomer ASC";
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds, "customer");
+            dgvcustomer.DataSource = ds;
+            dgvcustomer.DataMember = "customer";
+            dgvcustomer.ReadOnly = true;
+        }
+
+
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetdata();
+            showdata();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtid.Text == "" | txtnama.Text == "" | txtvoucher.Text == "")
+            {
+                MessageBox.Show("Semua data harus diisi", "Peringatan");
+                goto berhenti;
+            }
+
+            int num;
+
+            bool isNum = int.TryParse(txtvoucher.Text.ToString(), out num);
+
+            if (!isNum)
+            {
+                MessageBox.Show("Isi voucher harus angka", "peringatan");
+                goto berhenti;
+            }
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO customer values('" + txtid.Text + "','" + txtnama.Text + "'," + int.Parse(txtvoucher.Text) + ")";
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            resetdata();
+
+        berhenti:
+            ;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if(txtid.Text == "" | txtnama.Text == "" | txtvoucher.Text == "")
+            {
+                MessageBox.Show("Semua data harus diisi", "Peringaran");
+                goto berhenti;
+            }
+
+            int num;
+            bool isNum = int.TryParse(txtvoucher.Text.ToString(), out num);
+
+            if (!isNum)
+            {
+                MessageBox.Show("Isi voucher harus angka", "Peringatan");
+                goto berhenti;
+            }
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "UPDATE customer SET namaCustomer = '" + txtnama.Text + "', nominalIsi=" + txtvoucher.Text + "WHERE idCustomer = '" + txtid.Text + "'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            resetdata();
+
+        berhenti:
+            ;
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(txtid.Text == "")
+            {
+                MessageBox.Show("Isi dengan customer yang akan dihapus");
+                goto berhenti;
+            }
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "DELETE FROM customer WHERE idCustomer = '" + txtid.Text + "'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            resetdata();
+
+        berhenti:
+            ;
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM customer WHERE namaCustomer like '%" + txtcarinama.Text + "%'";
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds, "customer");
+            dgvcustomer.DataSource = ds;
+            dgvcustomer.DataMember = "customer";
+            dgvcustomer.ReadOnly = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            showdata();
+
+        }
+    }
+}
